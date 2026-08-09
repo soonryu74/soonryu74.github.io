57c671aa2898c082336fb4e15d6da0610c350433
soonryu74 <soonryu74@users.noreply.github.com>
2026-08-09 13:20:01 +0000

    deploy: 3950fe5ca5239a67e1c71216bde10e739e05f0c2

---
+++ data/complexes.js
@@ -0,0 +1,159 @@
+/* 갈매동 아파트 단지 실거래가 요약 (국토부 실거래가 공개시스템, 최근 1년, 매매) */
+const COMPLEXES = [
+ {
+  "name": "엘에이치이스트힐",
+  "count": 103,
+  "min": 40000,
+  "max": 67000,
+  "latest": {
+   "price": 60000,
+   "area": 52,
+   "floor": 2,
+   "date": "2026-07-21"
+  },
+  "areas": [
+   52,
+   60
+  ],
+  "buildYear": "2016"
+ },
+ {
+  "name": "갈매역아이파크",
+  "count": 76,
+  "min": 72000,
+  "max": 90000,
+  "latest": {
+   "price": 90000,
+   "area": 85,
+   "floor": 27,
+   "date": "2026-07-03"
+  },
+  "areas": [
+   85
+  ],
+  "buildYear": "2018"
+ },
+ {
+  "name": "한라비발디",
+  "count": 74,
+  "min": 59900,
+  "max": 79500,
+  "latest": {
+   "price": 77500,
+   "area": 85,
+   "floor": 4,
+   "date": "2026-07-02"
+  },
+  "areas": [
+   75,
+   85
+  ],
+  "buildYear": "2016"
+ },
+ {
+  "name": "구리갈매스타힐스",
+  "count": 67,
+  "min": 50250,
+  "max": 76500,
+  "latest": {
+   "price": 68000,
+   "area": 75,
+   "floor": 3,
+   "date": "2026-07-04"
+  },
+  "areas": [
+   75,
+   85
+  ],
+  "buildYear": "2017"
+ },
+ {
+  "name": "갈매6단지",
+  "count": 55,
+  "min": 35000,
+  "max": 75700,
+  "latest": {
+   "price": 75700,
+   "area": 85,
+   "floor": 17,
+   "date": "2026-07-30"
+  },
+  "areas": [
+   60,
+   75,
+   85
+  ],
+  "buildYear": "2018"
+ },
+ {
+  "name": "갈매더샵나인힐스",
+  "count": 43,
+  "min": 55000,
+  "max": 76300,
+  "latest": {
+   "price": 73300,
+   "area": 84,
+   "floor": 22,
+   "date": "2026-07-31"
+  },
+  "areas": [
+   70,
+   84,
+   85
+  ],
+  "buildYear": "2016"
+ },
+ {
+  "name": "구리갈매푸르지오",
+  "count": 32,
+  "min": 55400,
+  "max": 107300,
+  "latest": {
+   "price": 92700,
+   "area": 100,
+   "floor": 10,
+   "date": "2026-06-30"
+  },
+  "areas": [
+   84,
+   85,
+   100,
+   113
+  ],
+  "buildYear": "2017"
+ },
+ {
+  "name": "갈매와이시티",
+  "count": 25,
+  "min": 62500,
+  "max": 76500,
+  "latest": {
+   "price": 76500,
+   "area": 85,
+   "floor": 12,
+   "date": "2026-07-25"
+  },
+  "areas": [
+   75,
+   85
+  ],
+  "buildYear": "2017"
+ },
+ {
+  "name": "이룸넘버원리치안",
+  "count": 21,
+  "min": 8000,
+  "max": 16500,
+  "latest": {
+   "price": 8500,
+   "area": 15,
+   "floor": 7,
+   "date": "2026-07-07"
+  },
+  "areas": [
+   15,
+   30
+  ],
+  "buildYear": "2017"
+ }
+];
